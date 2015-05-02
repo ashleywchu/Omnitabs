b4582dc0f03e85f4ab515c66501884348515b0e6
Brian Choy <bycEEE@gmail.com>
2015-05-02 16:18:16 -0400

    Change directory structure

--- Chrome Extension/popup.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('#searchtabs').on( "submit", function(event) {
-    console.log("test");
-  });
-});
---
+++ popup.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#search_tabs_submit').click(function(event) {
+    event.preventDefault();
+    alert( "Handler for .submit() called." );
+  });
+});
