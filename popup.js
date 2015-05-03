623e4757983c6ef5da21928d38e1d12bdf10bd80
Brian Choy <bycEEE@gmail.com>
2015-05-03 17:54:53 -0400

    Add fuzzy search, get search tabs partially functional

--- popup.js
+++ popup.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $('#search_tabs_submit').click(function(event) {
-    event.preventDefault();
-    alert( "Handler for .submit() called." );
+  // Run runSearch function after keypress
+  $('#searchbox').on({
+    'keyup': runSearch
   });
 });
+
+// Search tab array
+function runSearch() {
+  searchString = $('#searchbox').val();
+  searchTabs(searchString, chrome.extension.getBackgroundPage().tabs);
+}
+
+// Returns fuzzy matched results
+function searchTabs(searchString, tabs) {
+  var results = fuzzy.filter(searchString, tabs);
+  var matches = results.map(function(el) { return el.string; });
+  $("#tabs-list").append(matches); 
+}
